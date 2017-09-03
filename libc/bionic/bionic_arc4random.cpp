f698794a8bc05ca2fa48c75247c1b3cba9884a6b
Daniel Micay <danielmicay@gmail.com>
2017-09-03 13:45:31 -0400

    use blocking getrandom to prevent urandom fallback
    It needs to be guaranteed that getentropy works in early boot for the
    hardened malloc in init. /dev/urandom isn't available so getrandom needs
    to block. This also guarantees that the entropy will be high quality.
    
    Change-Id: Idb64cbd857ea73910b9e3dc8462d7e01d53a2ec9

--- libc/bionic/bionic_arc4random.cpp
+++ libc/bionic/bionic_arc4random.cpp
@@ -41,7 +41,7 @@ void __libc_safe_arc4random_buf(void* buf, size_t n, KernelArgumentBlock& args) 
   // Only call arc4random_buf once we have `/dev/urandom` because getentropy(3)
   // will fall back to using `/dev/urandom` if getrandom(2) fails, and abort if
   // if can't use `/dev/urandom`.
-  static bool have_urandom = access("/dev/urandom", R_OK) == 0;
+  static bool have_urandom = true; //access("/dev/urandom", R_OK) == 0;
   if (have_urandom) {
     arc4random_buf(buf, n);
     return;
